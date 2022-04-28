d0aa2ba3d347c5e5d23c459df091c5493f03209c
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-04-28 10:01:48 -0500

    app routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {Routes, Route} from "react-router-dom"
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
@@ -7,7 +8,10 @@ function App() {
     return (
         <div>
             <Header />
-            <h1>Home Page</h1>
+            <Routes>
+                <Route path="/" element={<Photos />} />
+                <Route path="/cart" element={<Cart />}/>
+            </Routes>
         </div>
     )
 }
