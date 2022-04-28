e3ad41acbbb519c58b9bd6118e018e9d3ac3be27
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-04-28 11:56:16 -0500

    fetch photo

--- my-app/src/Context.js
+++ my-app/src/Context.js
@@ -1,9 +1,17 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 const Context = React.createContext()
+const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
 
 function ContextProvider({children}) {
     const [allPhotos, setAllPhotos] = useState([])
+    
+    useEffect(() => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setAllPhotos(data))
+    }, [])
+    
     return (
         <Context.Provider value={{allPhotos}}>
             {children}
