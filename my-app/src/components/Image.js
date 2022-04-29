631a2f730ebe97c02a06e9d0aaa5925747bbbec1
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-04-29 12:36:42 -0500

    header

--- my-app/src/components/Image.js
+++ my-app/src/components/Image.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 
 function Image({className , img}) {
     const [hovered, setHovered] = useState(false)
-    const {toggleFavorite, addToCart, cartItems} = useContext(Context)
+    const {toggleFavorite, addToCart, cartItems, removeFromCart} = useContext(Context)
     
 
     function heartIcon() {
@@ -14,11 +14,11 @@ function Image({className , img}) {
             return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
         }
     }
-    
+
     function cartIcon() {
         const alreadyInCart = cartItems.some(item => item.id === img.id)
         if(alreadyInCart) {
-            return <i className="ri-shopping-cart-fill cart"></i>
+            return <i className="ri-shopping-cart-fill cart" onClick={() => removeFromCart(img.id)}></i>
         } else if(hovered) {
             return <i className="ri-add-circle-line cart" onClick={() => addToCart(img)}></i>
         }
